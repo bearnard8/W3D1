8a8022027b7063a5cf1f717eabb4842bf71bdcd3
Stefano Bernardo <stefano.berna8@gmail.com>
2023-11-09 09:10:33 +0100

    Rev. 4 + js

---
+++ logic.js
@@ -0,0 +1,8 @@
+function goToTop() {
+    document.documentElement.scrollTop = 0;
+    window.alert("Forse è meglio se rileggi da capo...")
+}
+
+function youveGotIt()  {
+    window.alert("Complimenti!")
+}
